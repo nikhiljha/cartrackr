909ff83974485c5364b13bd432744b1757d1f4d4
Nikhil Jha <hi@nikhiljha.com>
2020-01-23 18:02:15 -0800

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+# GPS Tracker
+
+This is a fork of the original project, https://github.com/abradbury/Pycom-GPS-Tracking-System - WIP. The information below isn't accurate, but I may add some of it. I currently plan to primarily try sending via Sigfox, but if it can't, fallback to LTE.
+
+# ORIGINAL README
+
+An exploratory project with the aim of creating a GPS tracking device with no monthly costs. 
+
+The idea is for the GPS tracker to be placed inside a vehicle and when the vehicle starts up the tracker searches for an approved mobile telephone over Bluetooth. If the phone cannot be found, it assumes that the car is being stolen, alerts the user and begins tracking the car.
+
+There is an additional option to include a small battery such that the tracker can send a last reading when the vehicle's ignition is turned off. This would also allow potential use of the built-in accelerator to trigger a GPS message when movement is detected e.g. if the vehicle is being towed (the ignition wouldn't be on).
+
+## Usage
+1. Using Visual Studio, install the Pymakr plugin
+2. Connect the pytrack with attached FiPy to the computer
+3. Open the code in Visual Studio
+4. Right click on one of the project files and click upload
+
+## Components
+* [Pycom FiPy](https://pycom.io/product/fipy/) (when the UK's IOT LTE network capability is ready, then a [Pycom GPy](https://pycom.io/product/gpy/) can be used instead, as it is cheaper)
+* [Pycom pytrack](https://pycom.io/product/pytrack/) (includes GNSS capabilities and an accelerometer)
+* Sigfox antenna
+* Cellular antenna
+* A case/box such as the [Pycase](https://pycom.io/product/pycase-clear/)
+* A "hard-wire kit" to connect the tracker to your car's fuse box, for example, [the Nextbase one](https://www.amazon.co.uk/Nextbase-Camera-Hard-Wire-Kit/dp/B00XHYSQAC) (but you would need a Micro USB to Mini USB converter/adapter as Nextbase DashCams have a Mini USB input, but Pycom boards have Micro USB)
+
+Total component cost: around £100
+
+Total monthly cost: £0
+
+## Functionality
+### GPS
+Basic GPS readout has been implemented using [Pycom's libraries and examples](https://github.com/pycom/pycom-libraries/tree/master/pytrack).
+
+- The date printed is incorrect (1970), could this not be acquired from the GPS connection?
+- The Pycom library only supplies coordinates and time, other libraries may give more information such as accuracy and altitude
+- Why does the Pycom example need to setup an RTC beforehand?
+- Why does the Pycom example use the `gc` library?
+
+### Bluetooth
+TODO
+
+[Pycom Bluetooth API](https://docs.pycom.io/firmwareapi/pycom/network/bluetooth/)
+[Pycom Bluetooth tutorial](https://docs.pycom.io/tutorials/all/ble.html)
+
+It does not seem possible to uniquely identify an (iOS) phone using Bluetooth Low Energy, as this sort of thing is discouraged for privacy reasons. The advertised MAC address randomly changes. Cannot connect using the actual MAC address. It is possible to *temperamentally* list the services and characteristics the device offers. One possible solution would be to create an iOS app that sets some specific service UUID to search for, but there is some doubt over this as well.
+
+### Communications
+
+[Pycom SigFox API](https://docs.pycom.io/firmwareapi/pycom/network/sigfox.html)
+[Pycom SigFox tutorial](https://docs.pycom.io/tutorials/sigfox.html)
+
+The plan is to use SigFox until the UK's IOT LTE network capability is ready, then switch to using a Hologram.io SIM card. Communication rate will be limited as there are restrictions on the amount of data that can be send over these networks over a given time period. 
+
+- The SigFox connection is disappointingly poor where the car will be mainly based, with only 1 in 100 messages getting through, though elevation helps
+- To address this, have the SigFox server respond when a message is received - if it can (only 4 downlink messages a day are allowed), then the tracker can stop transmitting, unless the owner intervenes and would like to track the vehicle
+- Get callbacks/scheduling working
+
+- The SigFox limit is a regulatory limit, specifically 6 messages per hour
+- Downlink messages can only be sent when requested by the device
+- The simplest option, for now, would be to have the device continually transmit - but for more use, a battery should be added
+  - SigFox TX: 42 mA, idle: 62.7 mA, deep sleep: 24 uA
+  - How to enter deep sleep?
+  - How to measure voltage/current usage?
+
+### Monitoring
+The plan is to use either the free tier of Microsoft Azure IoT Central, or the Pybytes platform to view data sent from the Pycom boards, track the position of them and to send out alerts. The monitoring/tracking should be shareable, so, for example, the Police can use it to recover the stolen vehicle. 
+
+TODO
---
+++ main.py
@@ -0,0 +1,173 @@
+# pylint: disable=import-error
+
+import machine
+import math
+import network
+import socket
+import struct
+import pycom
+import os
+import time
+import utime
+import gc
+from machine import Timer
+from machine import RTC
+from L76GNSS import L76GNSS
+from pytrack import Pytrack
+
+class CommunicationInterface():
+    def __init__(self):
+        super().__init__()
+
+    def setup(self):
+        pass
+
+    def send(self, coordinates):
+        pass
+
+class WiFiCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up WiFi...")
+        print("WiFi set up")
+
+    def send(self, coordinates):
+        print("Sending: " + str(coordinates))
+
+class SigFoxCommunication(CommunicationInterface):
+    # https://docs.pycom.io/firmwareapi/pycom/network/sigfox.html
+
+    def setup(self):
+        print("Setting up SigFox...")
+        # RCZ1 to specify Europe, Oman & South Africa.
+        # RCZ2 for the USA, Mexico & Brazil.
+        # RCZ3 for Japan.
+        # RCZ4 for Australia, New Zealand, Singapore, Taiwan, Hong Kong, Colombia & Argentina.
+        sigfox = network.Sigfox(mode=network.Sigfox.SIGFOX, rcz=network.Sigfox.RCZ1)
+        self.sigfoxSocket = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+        self.sigfoxSocket.setblocking(True)
+        self.sigfoxSocket.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+        print("SigFox set up")
+
+    def send(self, coordinates):
+        dataToSend = struct.pack('>II', int(coordinates[0]*100000),  int(coordinates[1]*100000))
+        print("Sending: " + str(coordinates) + " over SigFox as " + str(dataToSend) + "(" + str(len(dataToSend)) + " bytes)...")
+
+        # Maximum data size is 12 bytes
+        # Up to 140 messages per day
+        pycom.rgbled(0xff00)
+        self.sigfoxSocket.send(dataToSend)
+        pycom.rgbled(0)
+
+        print("Sent")
+
+
+class CellularCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up Cellular...")
+        print("Cellular set up...")
+
+    def send(self, coordinates):
+        print("Sending: " + str(coordinates))
+
+
+class ConsoleCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up console...")
+        print("Console set up...")
+
+    def send(self, coordinates):
+        print("Sending: " + str(coordinates))
+        print("{}".format(coordinates))
+
+
+# Unfortunately, the abstract base class (abc) module is not available in MicroPython
+class GNSSInterface():
+    def __init__(self):
+        super().__init__()
+
+    def setup(self):
+        pass
+
+    def getCoordinates(self):
+        pass
+
+
+class PycomGNSS(GNSSInterface):
+    def setup(self):
+        time.sleep(2)
+        gc.enable()
+
+        # setup rtc
+        rtc = machine.RTC()
+        rtc.ntp_sync("pool.ntp.org")
+        utime.sleep_ms(750)
+        utime.timezone(7200)
+
+        py = Pytrack()
+        self.gnss = L76GNSS(py, timeout=30)
+
+        print("Waiting for GNSS connection...")
+        # Unfortunately, the tuple comparison method - cmp() - is not available in MicroPython
+        while(self.gnss.coordinates()[0] == None and self.gnss.coordinates()[1] == None):
+            pass
+
+        print("GNSS connection acquired")
+
+    def getCoordinates(self):
+        return self.gnss.coordinates()
+
+
+class CarTracker:
+    def __init__(self, communications, gnss, updateRate):
+        self.communications = communications
+        self.gnss = gnss
+        self.__alarm = Timer.Alarm(handler=None, s=updateRate, periodic=True)
+
+    def beginTracking(self):
+        self.setup()
+        self.sendLocationData()
+        self.__alarm.callback(handler=self.alarmCallback, arg=None)
+
+    def setup(self):
+        # Turn off network services that are not needed
+        network.Bluetooth().deinit()
+        network.WLAN().deinit()
+        network.LTE().deinit()
+
+        self.gnss.setup()
+        for communication in self.communications:
+            communication.setup()
+
+    def logCoordinates(self, datetime, coordinates):
+        with open("/flash/locations.txt", "a+") as location_file:
+            # 2019-05-22 11:41:19 (51.80332, -0.17852)
+            location_file.write(str(datetime) + " " + str(coordinates) + "\n")
+        # To read the written data:
+        #   with open("/flash/locations.txt", "r+") as locations_file:
+        #       locations_file.read().split("\n")
+        # TODO: Check that the file system does not get full, delete oldest/first lines from file if this happens
+        #   Useful commands to do this: 
+        #       os.getfree("/flash") # in KiB? e.g. 4036
+        #       os.listdir("/flash")
+        #       os.stat("/flash/main.py") # 7 tuple is size in bytes e.g. 4345 bytes = 4 KB
+
+    def alarmCallback(self, alarm):
+        self.sendLocationData()
+
+    def sendLocationData(self):
+        coordinates = self.gnss.getCoordinates()
+        for communication in self.communications:
+            communication.send(coordinates)
+            # The GNSS system will know the datetime, but the Pycom library does not expose this. The device
+            # itself has no concent of time, other than local time, which isn't useful. 
+            self.logCoordinates("???", coordinates)
+
+pycom.heartbeat(False)
+pycom.rgbled(0xFFBF00)
+
+communications = [WiFiCommunication()]
+gnss = PycomGNSS()
+updateRate = 30 # Seconds
+
+carTracker = CarTracker(communications, gnss, updateRate)
+carTracker.beginTracking()
